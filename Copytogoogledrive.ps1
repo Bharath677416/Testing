c35ea025563bd3a9dd2e913eefbb51e894b1193b
Bharath677416 <143838857+Bharath677416@users.noreply.github.com>
2023-09-02 00:32:00 +0530

    first version to copyfiles

---
+++ Copytogoogledrive.ps1
@@ -0,0 +1,45 @@
+﻿#############################################################
+## Script to copy the data from Local hardisk to google Drive
+## Transscript logs
+#############################################################
+
+
+##### Checking the path #####################################
+
+$Sources = Get-Content "C:\Scripts\Drive_Sync\Input.txt"
+$Destination = "F:\Other computers\My Computer\Drive_Sync"
+$scriptLogs = "K:\Google Drive logs\"
+
+##### Start Logs files ######################################
+
+$date = (Get-Date).ToString(“yyyyMMdd_HHmmss”)
+
+Start-Transcript -Path $scriptLogs\"Logs"_$date.log
+
+
+$Testpath = $Sources[0], $Sources[1], $Destination, $scriptLogs
+$Testpaths = $Testpath -split ","
+foreach($path in $Testpaths)
+ {
+  if(!(Test-path -Path $path))
+    {
+      $message = $path + " Is not available pls check"
+      Write-Output $message
+    }
+break
+ }
+
+##### Starting the copy  #####################################
+
+Foreach($Source in $Sources)
+ {
+ robocopy $Source $Destination /E
+ }
+
+ ##### Removing old log files ###############################
+
+$limit = (Get-Date).AddDays(-3)
+
+Get-ChildItem $scriptLogs -Recurse | ? { -not $_.PSIsContainer -and $_.CreationTime -lt $limit} | Remove-Item
+
+Stop-Transcript
